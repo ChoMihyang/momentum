2e983eaa4a3dd8fa1e939ececed1c2e2f9d2f441
Cho-Mihyang <algid8252@g.yju.ac.kr>
2022-02-08 20:18:24 +0900

    [LOGIN] Saving Username - localStorage

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const greeting          = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME  = "hidden";
 
 function onLoginSubmit(event){
-    event.preventDefault();                         // 기본 동작 실행 막기
+    event.preventDefault();                         // 브라우저의 기본 동작 실행 막기
     const username = loginInput.value;              // 유저 이름 저장
+    localStorage.setItem("username", username);     // Storage 내 유저 정보 저장
     loginForm.classList.add(HIDDEN_CLASSNAME);      // hidden이라는 class name 추가
     greeting.innerText = `Hello ${username}`;       // id name이 'greeting'인 h1 태그 내 username을 추가  
     greeting.classList.remove(HIDDEN_CLASSNAME);    // h1의 class name을 제거함으로써 화면에 표시되도록 함
